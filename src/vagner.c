868df7e31e505a3f098cbe126205bf98c5b5f255
Vagner Machado <VagnerMachado84@hotmail.com>
2020-03-17 21:34:49 -0400

    Updated readme.md and added comments to main file

--- src/vagner.c
+++ src/vagner.c
@@ -1,9 +1,39 @@
 /*
- * vagner.c
+ * 		vagner.c
  *
- *  Created on: Mar 13, 2020
- *      Author: vagner
+ *  	Created on: Mar 13, 2020
+ *
+ *      Author: Vagner Machado
+ *      		QC ID 23651127
+ *      		CSC 340 - Queens College
+ *      		Professor John Svadlenka
+ *
+ 	 	 	 	 *** This program accomplishes ***
+
+			0. The program generates 10,000 random integers.
+			1. The program finds the lowest and maximum values using a single thread and prints the values.
+			2. Given the parameter -printInput as parameter, the program prints the 10,000 random integers to file.
+			3. The main process creates 5 threads and each is responsible for 2,000 of those random integers.
+			4. Each of the 5 threads, creates 20 threads to find the lowest value in 100 of those random integers.
+			5. This will find the 100 lowest values, 20 in each parent thread.
+			6. Each parent thread finds the lowest of its 20 integers, so there are 5 lowest values left.
+			7. The lowest 20 values for each child thread are printed.
+			8. The lowest 5 values for the parent threads are printed.
+			9. The lowest value out of lowest 5 is printed.
+			10. Main thread scans the 5 values left to find the lowest value.
+			11. All output is printed to file vagner-output.txt.
+			NOTE: User can pass parameter -printInput to see all generated integers printed to file.
+
+				***   COMPILATION INSTRUCTIONS   ***
+
+			gcc -c vagner.c                  // compiles the file to object file.
+			gcc -o app.exe vagner.o          // links object file to executable.
+			OPTION 1: ./app.exe              // runs the program without printing the random integers.
+			OPTION 2: ./app.exe -printInput  // runs the program printing the random input integers.
+			OPTION 3: ./app.exe -help        // runs the program, prints these instructions to console and vagner-output.txt file.;
  */
+
+//libraries
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
